5b1f98bb43626b19765ec4d68699cb3afb4a7060
ikabeee <carlglz30@gmail.com>
2024-12-15 14:21:10 -0500

    List of packages

---
+++ get_hypr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+List of packages that will be installed
+
+Window Manager: Hyprland/ Wayland
+Notification daemon: Mako
+Handling audio: Pipewire
+Desktop portal: xdg-desktop-portal-hyprland
+Panel: Hyprpanel
+Display Manager: LightDM
+Greeter: Webkit2
+Logout Menu: Wlogout
+Wallpaper tool: Swaybg
+Swappy
+Hyprpaper
+Git
+Wofi
+Dolphin
+yay
+Alacritty
+xfce4-settings
+xfce4
+
+Swaylock
+Clipboard Manager: cliphist
+Fastfetch
+
+
+
+---FONTS---
+ttf-cascadia-code
+ttf-fira-code
+noto-fonts-emoji
