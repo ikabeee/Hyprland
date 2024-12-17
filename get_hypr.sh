9353a972a06d8e586c587ce9e0506a729c382dd4
ikabeee <carlglz30@gmail.com>
2024-12-16 20:31:12 -0500

    ADD: Installation script

--- get_hypr.sh
+++ get_hypr.sh
@@ -50,21 +50,119 @@
 # ttf-fira-code
 # noto-fonts-emoji
 
-# Installing yay
+PACKAGES=(
+  "hyprland"
+  "uwsm"
+  "mako"
+  "pipewire"
+  "lxappearance"
+  "numix-icon-theme"
+  "xcursor-breeze"
+  "xdg-desktop-portal-hyprland"
+  "hyprpanel"
+  "hyprsome-git"
+  "hyprpicker"
+  "lightdm"
+  "lightdm-webkit2-greeter"
+  "wlogout"
+  "iwgtk"
+  "blueberry"
+  "overskride"
+  "network-manager-applet"
+  "udiskie"
+  "swaybg"
+  "hyprpaper"
+  "wofi"
+  "dolphin"
+  "alacritty-git"
+  "cliphist"
+  "wireplumber-git"
+  "hyprshot"
+  "swaylock"
+  "xfce4-settings"
+  "fastfetch"
+  "nano-git"
+  "vim-git"
+  "visual-studio-code-bin"
+  "brave-bin"
+  "firefox-developer-edition"
+  "docker-desktop"
+  "dbeaver"
+  "ttf-cascadia-code"
+  "ttf-fira-code"
+  "noto-fonts-emoji"
+)
 
-echo "Yay installation"
 
-# Verify if user has root privileges
+# Verify permission
 
 if [[ $EUID -eq 0 ]] then;
     echo "Do not execute this script as root. Please, use sudo privileges"
     exit 1
 fi
 
+# Installing yay
+
+echo "Yay installation"
+
+# Verify if user has root privileges
+
+echo "First step: Git installation"
+
 # Checking for git existence
 
-if ! command -v git &&> /dev/null; then
-    echo "Installing git.\n"
+if ! command -v git &> /dev/null; then
+    echo -e "Installing git.\n"
     sudo pacman -S --needed --noconfirm git
 else
-    echo "Git is already installed
+    echo -e "Git is already installed.\n"
+fi 
+
+# Installing base-devel
+
+echo -e "Installing base-devel.\n"
+
+if ! pacman -Q base-devel &> /dev/null; then
+    echo -e "Installing base-devel.\n"
+    sudo pacman -S --needed --noconfirm base-devel
+else
+    echo -e "Base-devel already installed.\n"
+fi
+
+# Installing yay
+
+echo -e "Installing yay from aur repositories.\n"
+
+git clone https://aur.archlinux.org/yay.git || {
+    echo -e "Error: Your computer cannot clone the repository, please verify your internet connection.\n"
+    exit 1
+}
+
+#Getting into yay directory
+cd yay || {
+    echo -e "Error: The cloned directory could not be accessed.\n"
+    exit 1
+}
+
+echo -e "Compiling yay.\n"
+makepkg -si --noconfirm || {
+    echo -e "Error: Yay could not be installed for any reason"
+    exit 1
+}
+
+cd ..
+rm -rf yay
+
+echo -e "Basic config has been applied"
+
+# Install packages
+
+read -n1 -rep "Would you like to install the following packages?"
+
+echo -e "List of packages: \n"
+
+for package in "${PACKAGES[@]}"; do 
+    echo -e "$package \n"
+for package in "${PACKAGES[@]}"; do
+    echo -e "Installing $package... \n"
+    done
